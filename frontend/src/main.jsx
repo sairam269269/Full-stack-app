de1226d2121950a37e31fb230f14604ed54bae1c
sairam269269 <sairam.s269269@gmail.com>
2025-05-05 13:56:19 +0530

    Implement Redux for user authentication and add navigation links

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/features/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />} />)
 );
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+ReactDom.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
--- frontend/src/pages/auth/Navigation.jsx
+++ frontend/src/pages/auth/Navigation.jsx
@@ -8,11 +8,122 @@ import {
   AiOutlineShoppingCart,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSclice.js";
+import { logout } from "../../redux/features/auth/authSlice.js";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const { userInfo } = useSelector((state) => state.auth);
+  const [dropDownbar, setDropDownbar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleDropDown = () => {
+    setDropDownbar(!dropDownbar);
+  };
+
+  const toggleClosebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const closeSidebar = () => {
+    setShowSidebar(false);
+  };
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLoginMutation();
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div
+      style={{ zIndex: 9999 }}
+      className={`${
+        showSidebar ? "hidden" : "flex"
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      id="navigation-container"
+    >
+      <div className="flex flex-col justify-center space-y-4">
+        <Link
+          to="/"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+        </Link>
+
+        <Link
+          to="/shop"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
+        </Link>
+
+        <Link
+          to="/cart"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">CART</span>{" "}
+        </Link>
+
+        <Link
+          to="/favorite"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <FaHeart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>{" "}
+        </Link>
+      </div>
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-800 focus:outline-none"
+        ></button>
+
+        {userInfo ? (
+          <span className="text-white">{userInfo.username}</span>
+        ) : (
+          <></>
+        )}
+      </div>
+      <ul>
+        <li>
+          <Link
+            to="/login"
+            className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
+            <span className="hidden nav-item-name">LOGIN</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineUserAdd size={26} />
+            <span className="hidden nav-item-name">REGISTER</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
